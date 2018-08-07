021b982711884f6963b84d526f32be10e0a1a633
baolina <569681517@qq.com>
2018-08-07 19:31:34 +0800

    登录请求完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,5 @@
 $(function(){
-  // 表单校验
+  // 表单初始化校验
   $('#form').bootstrapValidator({
 
     // 指定校验时的图标显示，默认是bootstrap风格
@@ -16,27 +16,58 @@ $(function(){
           },
           //长度校验
           stringLength: {
-          min: 2,
-          max: 6,
-          message: '用户名长度必须在2到6之间'
+              min: 2,
+              max: 6,
+              message: '用户名长度必须在2到6之间'
           },
+          callback:{
+            message:"用户名不存在"
+          }
         }
       },
       password:{
         validators:{
           notEmpty: {
-            message: '密码不能为空'
+            message: '密码不能为空!'
           },
           //长度校验
           stringLength: {
-          min: 6,
-          max: 12,
-          message: '用户名长度必须在6到12之间'
+            min: 6,
+            max: 12,
+            message: '用户名长度必须在6到12之间'
           },
+          callback:{
+            message:"密码错误!"
+          }
         }
       }
     }
   })
   // 登录请求
+  $('#form').on('success.form.bv',function(e){
+    e.preventDefault();
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data:$('#form').serialize(),
+      dataType:"json",
+      success:function(info){
+        console.log(info);
+
+        if(info.success) {
+          location.href = "index.html"
+        }
+
+        if(info.error === 1000){
+          $("#form").data('bootstrapValidator').updateStatus("username","INVALID","callback");
+        }
+        if(info.error === 1001){
+          $("#form").data('bootstrapValidator').updateStatus("password","INVALID","callback");
+        }
+      }
+    })
+  })
   
 })
+
+
